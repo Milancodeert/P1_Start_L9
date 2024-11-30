361a0f93927b7c0b883430ccc8993041b6bcf993
Milancodeert <milan.stassen2012@gmail.com>
2024-11-30 10:44:11 +0100

    p1_9

--- startcode/main.py
+++ startcode/main.py
@@ -1,12 +1,48 @@
 import pygame, sys
 from pygame.locals import QUIT
-
+eersteCombinatie = int(input("wat wil je als achtergrond hebben? (tik de eerste kleur in van de kleur in python) "))
+tweedeCombinatie = int(input("tik hetzelfde in, maar dan de tweede combinatie. "))
+derdeCombinatie = int(input("tik hetzelfde in, maar dan de derde combinatie. "))
 pygame.init()
-venster = pygame.display.set_mode((200, 50))
+venster = pygame.display.set_mode((600, 500))
+venster.fill((eersteCombinatie,tweedeCombinatie,derdeCombinatie))
+tekengrootte = 10
+#def extraGrootte():
+ #   if event.type == pygame.MOUSEWHEEL:
+  #      if event.y == -1:
+   #         tekengrootte -= 1
+
+
+
+
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if pygame.mouse.get_pressed() == (True,False,False):
+            pygame.draw.circle(venster,color,pygame.mouse.get_pos(),tekengrootte)
+        if pygame.mouse.get_pressed() == (False,False,True):
+            pygame.draw.circle(venster,(eersteCombinatie,tweedeCombinatie,derdeCombinatie),pygame.mouse.get_pos(),10)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                color = (255,0,0)
+            if event.key == pygame.K_s:
+                pygame.image.save(venster,"tekening" + ".jpg")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                color = (0, 0, 0)
+            if event.key == pygame.K_s:
+                pygame.image.save(venster, "tekening" + ".jpg")
+        if event.type == pygame.MOUSEWHEEL:
+            if event.y == -1:
+                tekengrootte -= 1
+        if event.type == pygame.MOUSEWHEEL:
+            if event.y == +1:
+                tekengrootte += 1
+
+
     pygame.display.update()
